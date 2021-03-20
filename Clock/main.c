de324fc89dedd7a23b6645afbc1acb2826851304
cmcta82 <cmcta82@gmail.com>
2021-03-20 16:39:40 +0100

    First try

--- Clock/main.c
+++ Clock/main.c
@@ -1,14 +1,62 @@
-//
-//  main.c
-//  Clock
-//
-//  Created by Carlos Tavares on 20/03/2021.
-//
+/*
+ Author: Carlos Tavares
+ Date: 20/03/21
+ Purpose: Create a clock in C
+ 
+ .'`~~~~~~~~~~~`'.
+ (  .'11 12 1'.  )
+ |  :10 \    2:  |
+ |  :9   @-> 3:  |
+ |  :8       4;  |
+ '. '..7 6 5..' .'
+  ~-------------~
+    
+ */
+
 
 #include <stdio.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+
+void testPrint(void);
+
+
+int main(int argc, char *argv[]){
+    
+    char clock[7][17] = {
+        { .'`~~~~~~~~~~~`'.},
+        { (  .'11 12 1'.  )},
+        { |  :10 \    2:  |},
+        { |  :9   @-> 3:  |},
+        { |  :8       4;  |},
+        { '. '..7 6 5..' .'},
+        {  ~-------------~ },
+
+    };
+
+    
     return 0;
+}
+
+void testPrint(){
+    printf("\n");
+    printf(" .'`~~~~~~~~~~~`'.");
+    printf("\n");
+
+    printf(" (  .'11 12 1'.  )");
+    printf("\n");
+
+    printf(" |  :10 \\    2:  |");
+    printf("\n");
+
+    printf(" |  :9   @-> 3:  |");
+    printf("\n");
+
+    printf(" |  :8       4;  |");
+    printf("\n");
+
+    printf(" '. '..7 6 5..' .'");
+    printf("\n");
+
+    printf("  ~-------------~");
+    printf("\n");
 }
