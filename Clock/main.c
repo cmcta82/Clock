862644f82202924ae301defb60329fda6bac2415
cmcta82 <cmcta82@gmail.com>
2021-03-20 16:18:52 +0100

    Initial Commit

---
+++ Clock/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Clock
+//
+//  Created by Carlos Tavares on 20/03/2021.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
